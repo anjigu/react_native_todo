fbf354b5e3b0a5f188ad41988c213fb1bd632f36
안지수 <anjisu@anjisuui-MacBookPro.local>
2023-02-03 17:22:36 +0900

    feat: input, placeholder생성

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -15,3 +15,15 @@ const SignInScreen = () => {
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
+export default SignInScreen;
