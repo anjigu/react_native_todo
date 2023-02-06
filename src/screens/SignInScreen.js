e6ff5637e3b4bcff887979b31c0b07910d989889
안지수 <anjisu@anjisuui-MacBookPro.local>
2023-02-06 11:35:59 +0900

    feat: keyboardavoidingview - safeinputview 컴포넌트화

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,39 +1,34 @@
-import { Image, Keyboard, KeyboardAvoidingView, Platform, Pressable, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import Input, { KeyboardTypes, ReturnKeyTypes } from '../components/Input';
+import SafeInputView from '../components/SafeInputView';
 
 const SignInScreen = () => {
   return (
-    <KeyboardAvoidingView 
-    style={styles.avoid}
-    behavior={Platform.select({ ios: 'padding' })}
-    >
-      {/*8, 9번째 코드 동일한 것*/}
-      <Pressable 
-      style={styles.avoid} 
-      onPress={Keyboard.dismiss}
-      >
-      {/* <Pressable style={styles.avoid} onPress={() => Keyboard.dismiss()}> */}
-    <View style={styles.container}>
-      <Image
-        source={require('../../assets/main.png')}
-        style={styles.image}
-        resizeMode={'cover'}
-      />
+    <SafeInputView>
+      <View style={styles.container}>
+        <Image
+          source={require('../../assets/main.png')}
+          style={styles.image}
+          resizeMode={'cover'}
+        />
 
-      <Input
-        title={'email'}
-        placeholder={'your@email.com'}
-        keyboardType={KeyboardTypes.EMAIL}
-        returnKeyType={ReturnKeyTypes.NEXT}
-      />
-      <Input title={'password'} secureTextEntry placeholderTextColor={'red'} />
-    </View>
-    </Pressable>
-    </KeyboardAvoidingView>
+        <Input
+          title={'email'}
+          placeholder={'your@email.com'}
+          keyboardType={KeyboardTypes.EMAIL}
+          returnKeyType={ReturnKeyTypes.NEXT}
+        />
+        <Input
+          title={'password'}
+          secureTextEntry
+          placeholderTextColor={'red'}
+        />
+      </View>
+    </SafeInputView>
   );
 };
+
 const styles = StyleSheet.create({
-  avoid: {flex : 1},
   container: {
     flex: 1,
     justifyContent: 'center',
