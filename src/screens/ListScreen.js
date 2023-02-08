e654ab0ee7fc886e2067b19689ff226464fe12a0
안지수 <anjisu@anjisuui-MacBookPro.local>
2023-02-08 14:06:47 +0900

    fix: 사용하지 않는 코드 정리

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,18 +1,11 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-const ListScreen = ({ navigation }) => {
+const ListScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 30 }}>ListScreen</Text>
     
-      <Button
-        title="push" //같은 화면이어도, 화면을 계속 쌓으면서 이동
-        onPress={() => navigation.push('List', { ts: Date.now() })}
-      />
-      <Button
-        title="navigate" //같은 화면이면, 추가로 화면을 쌓지 않음
-        onPress={() => navigation.navigate('List', { ts: Date.now() })}
-      />
+    
     </View>
   );
 };
